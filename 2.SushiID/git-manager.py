b02b9a08012ca6fcb716c9fe187e3b7615a3af1d
tianji <tj@hhttech.com>
2016-06-17 02:16:03 +0800

    add init to git-manager

--- 2.SushiID/git-manager.py
+++ 2.SushiID/git-manager.py
@@ -56,15 +56,48 @@ def production_sync(repo, msg = ""):
     
     print_success()
 
+def production_init(repo_url, local_path):
+    if os.path.exists(local_path):
+        print "Path '%s' already exists. Use another name or delete the old one. Abort" % local_path
+        return
+
+    repo = Repo.init(local_path)
+    origin = repo.create_remote('origin', repo_url)
+    origin.fetch(progress=MyProgressPrinter())
+    
+    print "Please choose from the following branches:"
+    for i in range(0, len(origin.refs)):
+        print "    %d : " % i + origin.refs[i].name
+    b = int(raw_input("Input the brance number:"))
+    if b > len(origin.refs):
+        print "Input wrong. Abort"
+        return
+    
+    branch_name = origin.refs[b].name.split("/")
+    branch_name = branch_name[-1]
+    # Setup a local tracking branch of a remote branch
+    h = repo.create_head(branch_name, origin.refs[b])
+    h.set_tracking_branch(origin.refs[b])
+    repo.head.reference = h
+
 def main():
     if (len(sys.argv) < 2):
         print "%s sync" % sys.argv[0]
+        print "%s init repo_name" % sys.argv[0]
         return
     
-    repo = Repo(os.path.dirname(os.path.realpath(__file__)) + "\\..\\")
-    
     if sys.argv[1] == 'sync':
+        try:
+            repo = Repo(os.path.dirname(os.path.realpath(__file__)) + "\\..\\")
+        except:
+            print "Current dir is not a git repo."
+            return
         production_sync(repo)
+    elif sys.argv[1] == 'init':
+        prod_repo_name = sys.argv[2]
+        repo_url = "git@git.huantengsmart.com:production/%s.git" % prod_repo_name
+        repo_local_path = prod_repo_name
+        production_init(repo_url, repo_local_path)
 
 if __name__ == "__main__":
     main()
